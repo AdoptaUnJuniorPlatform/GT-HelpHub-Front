8d7c8c887a4841ef5a5fb903796afc4fb8340c4f
Gloria R <norimaqidoe@hotmail.com>
2024-10-08 00:31:35 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' h-screen w-full flex justify-center items-center text-violet-700'>Help Hub</h1>
+    </>
+  )
+}
+
+export default App
