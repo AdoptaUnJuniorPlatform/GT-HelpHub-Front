2d9954b7fef677c1e9ae259de137c9a4d7085b16
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-10-13 00:07:09 +0100

    feat: add OfficialLogo component

---
+++ src/components/OfficialLogo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelpHubFullSteps from '../assets/HelpHubFullSteps.svg';
+
+
+const OfficialLogo: React.FC = () => {
+  return (
+    <div>
+      <img src={HelpHubFullSteps} alt="Help Hub Full Steps" />
+    </div>
+  );
+};
+
+export default OfficialLogo;
