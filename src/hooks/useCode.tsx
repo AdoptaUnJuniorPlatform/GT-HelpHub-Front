5602d5c8c6bff066f3ec41a0f59215642d56e0c7
Gloria R <norimaqidoe@hotmail.com>
2024-10-24 14:09:13 -0600

    feat: Creation of useCode hook to generate 6 digit random code

---
+++ src/hooks/useCode.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+function useCode() {
+  const [code, setCode] = useState<string>('');
+
+  const generateCode = () => {
+    const newCode = Math.floor(100000 + Math.random() * 900000).toString();
+    setCode(newCode);
+  };
+  
+  useEffect(() => {
+    generateCode(); 
+  }, []);
+
+  return { code, generateCode };
+}
+
+export default useCode
