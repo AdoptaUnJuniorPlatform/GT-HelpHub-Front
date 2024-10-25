886bcf184122752de236f5c573cd3925b3aefa07
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-10-25 19:27:20 +0100

    chore: fix indent

--- src/components/UserRegistrationStep1.tsx
+++ src/components/UserRegistrationStep1.tsx
@@ -46,49 +46,46 @@ const UserRegistrationStep1: React.FC<UserRegistrationStep1Props> = ({
       currentStep={currentStep}
     >
       
-    {/* Contenido específico del paso 1 */}
-    <div className="relative w-[571px] h-[140px] top-[-30px] left-[-160px] mt-[132px] mx-auto bg-gray-100 rounded-md border border-[#b7b7b7]">
+      {/* Contenido específico del paso 1 */}
+      <div className="relative w-[571px] h-[140px] top-[-30px] left-[-160px] mt-[132px] mx-auto bg-gray-100 rounded-md border border-[#b7b7b7]">
       
-      {/* Texto "Sobre mí" con el rectángulo pequeño */}
-      <div className="absolute top-[-10px] left-[20px] bg-gray-100 px-2">
-        <div className="text-[#696868] text-[15px] font-normal leading-normal tracking-wide">
-          Sobre mí
+        {/* Texto "Sobre mí" con el rectángulo pequeño */}
+        <div className="absolute top-[-10px] left-[20px] bg-gray-100 px-2">
+          <div className="text-[#696868] text-[15px] font-normal leading-normal tracking-wide">Sobre mí</div>
+        </div>
+
+        {/* Textarea en el rectángulo grande */}
+        <textarea
+          className="w-[526px] h-[150px] mt-[10px] mx-auto p-4 border border-gray-300 rounded-md text-[#696868] text-base font-normal tracking-tight resize-none overflow-hidden bg-transparent"
+          placeholder="Por Ej: Soy una joven estudiante de enfermería, tengo 22 años vivo en Madrid con unas amigas. Soy una apasionada por la música, y que desea aprender a tocar el piano."
+          maxLength={255}
+          style={{ border: 'none', outline: 'none' }}
+          onChange={handleTextareaChange}
+        />
+
+        {/* Contador de caracteres */}
+        <div className="absolute right-[15px] bottom-[10px] text-[#b7b7b7] text-base font-normal">
+          {text.length}/255
         </div>
       </div>
 
-      {/* Textarea en el rectángulo grande */}
-      <textarea
-        className="w-[526px] h-[150px] mt-[10px] mx-auto p-4 border border-gray-300 rounded-md text-[#696868] text-base font-normal tracking-tight resize-none overflow-hidden bg-transparent"
-        placeholder="Por Ej: Soy una joven estudiante de enfermería, tengo 22 años vivo en Madrid con unas amigas. Soy una apasionada por la música, y que desea aprender a tocar el piano."
-        maxLength={255}
-        style={{ border: 'none', outline: 'none' }}
-        onChange={handleTextareaChange}
-      />
+      {/* Código postal */}
+      <div className="relative w-[571px] top-[-50px] mt-8 left-[-160px] mx-auto">
+        <label className="text-[#434242] text-2xl font-normal font-['Roboto']">Ubicación</label>
+        <input 
+          className={`w-full h-[40px] mt-2 p-4 border rounded-md text-[#696868] text-base font-normal tracking-tight focus:outline-none ${
+            postalCodeError ? 'border-red-500' : 'border-gray-300'}`}
+          type="text"
+          inputMode="numeric"
+          pattern="[0-9]*"
+          placeholder="Código postal (CP)"
+          value={postalCode}
+          onChange={handlePostalCodeChange}
+          maxLength={5}
+        />
 
-      {/* Contador de caracteres */}
-      <div className="absolute right-[15px] bottom-[10px] text-[#b7b7b7] text-base font-normal">
-      {text.length}/255
-      </div>
-    </div>
-
-    {/* Código postal */}
-    <div className="relative w-[571px] top-[-50px] mt-8 left-[-160px] mx-auto">
-      <label className="text-[#434242] text-2xl font-normal font-['Roboto']">Ubicación</label>
-      <input 
-        className={`w-full h-[40px] mt-2 p-4 border rounded-md text-[#696868] text-base font-normal tracking-tight focus:outline-none ${
-          postalCodeError ? 'border-red-500' : 'border-gray-300'
-        }`}
-        type="text"
-        inputMode="numeric"
-        pattern="[0-9]*"
-        placeholder="Código postal (CP)"
-        value={postalCode}
-        onChange={handlePostalCodeChange}
-        maxLength={5}
-      />
-
-      {/* Texto debajo del input */}
-      {postalCodeError ? (
+        {/* Texto debajo del input */}
+        {postalCodeError ? (
           <p className="text-red-500 text-base font-normal mt-2">
             Revisa el código postal, debe tener 5 dígitos.
           </p>
@@ -97,7 +94,7 @@ const UserRegistrationStep1: React.FC<UserRegistrationStep1Props> = ({
             Escribe tu código postal (5 dígitos) de tu lugar de residencia.
           </p>
         )}
-    </div>
+      </div>
     </Layout>
   );
 };
