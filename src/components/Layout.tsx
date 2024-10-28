ed745b378d3d0b62f8462157e6e81470fd212f89
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-10-28 00:36:34 +0000

    feat: add new prop for step 4

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { FC} from 'react';
+import { FC } from 'react';
 import LeftColumn from './LeftColumn'; 
 import RightColumn from './RightColumn';
 
@@ -12,13 +12,33 @@ interface LayoutProps {
   currentStep: number;
   steps: string[];
   children: React.ReactNode;
+  showExtraContent?: boolean; // Nueva prop para controlar si se muestra contenido extra en LeftColumn
+  extraContent?: React.ReactNode; // Nueva prop para pasar contenido adicional específico
 }
 
-const Layout: FC<LayoutProps> = ({ title, description, stepTitle, stepDescription, onBackClick, onNextClick, currentStep, steps, children }) => {
+const Layout: FC<LayoutProps> = ({ 
+  title, 
+  description, 
+  stepTitle, 
+  stepDescription, 
+  onBackClick, 
+  onNextClick, 
+  currentStep, 
+  steps, 
+  children, 
+  showExtraContent = false, 
+  extraContent 
+}) => {
   return (
-    <div className="flex h-screen bg-white">
+    <div className="flex h-screen">
       {/* Columna Izquierda */}
-      <LeftColumn title={title} description={description} />           
+      <LeftColumn 
+        title={title} 
+        description={description} 
+        showExtraContent={showExtraContent} // Pasamos la prop
+        extraContent={extraContent} // Pasamos el contenido adicional
+      />           
+
       {/* Columna Derecha */}          
       <RightColumn
         stepTitle={stepTitle}
@@ -26,8 +46,11 @@ const Layout: FC<LayoutProps> = ({ title, description, stepTitle, stepDescriptio
         onBackClick={onBackClick}
         onNextClick={onNextClick}
         currentStep={currentStep}
-        steps={steps}>
-        { children }</RightColumn></div>
+        steps={steps}
+      >
+        {children}
+      </RightColumn>
+    </div>
   );
 };
 
