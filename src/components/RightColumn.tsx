a9172859ec25e026d9cba8761a90567cd12cff8e
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-11-06 22:14:37 +0000

    feat: add props for conditional in step 4 to render form with data or empty form

--- src/components/RightColumn.tsx
+++ src/components/RightColumn.tsx
@@ -10,13 +10,14 @@ interface RightColumnProps {
   steps: string[];
   currentStep: number;
   children: React.ReactNode;
+  isSubmitted?: boolean;
 }
 
-const RightColumn: React.FC<RightColumnProps> = ({ onBackClick, onNextClick, stepTitle, stepDescription, steps, currentStep, children }) => {
+const RightColumn: React.FC<RightColumnProps> = ({ onBackClick, onNextClick, stepTitle, stepDescription, steps, currentStep, children, isSubmitted = false }) => {
   
   return (
     <>
-      <div className="w-2/3 p-10 bg-gray-100 flex items-center justify-center relative p-10">
+      <div className="w-2/3 p-10 bg-beutral-50 flex items-center justify-center relative p-10">
         {/* Barra de progreso */}
         <ProgressBar currentStep={currentStep} steps={steps}/>
 
@@ -34,9 +35,25 @@ const RightColumn: React.FC<RightColumnProps> = ({ onBackClick, onNextClick, ste
         <div className="absolute w-full top-[120px] left-[116px] mb-10 px-10">{children}</div>
 
         {/* Botones comunes */}
-        <div className="absolute top-[575px] left-[-55px] bottom-20 w-full flex justify-between px-16">
-          <Button texto="Atrás" color="text-[#b7b7b7]" onClick={onBackClick} className="ml-[115px]" />  
-          <Button texto="Siguiente" color="text-[#496ceb]" onClick={onNextClick} className="mr-[75px]" />
+        <div
+          className={`absolute ${
+            currentStep === 4 
+              ? (isSubmitted ? 'top-[1730px] bottom-8' : 'top-[1420px] bottom-8') // Step 4 con condicional de isSubmitted
+              : 'top-[575px]' // Otros pasos
+          } left-[-55px] bottom-20 w-full flex justify-between px-16`}
+        >
+          <Button 
+            texto="Atrás" 
+            color="text-[#b7b7b7]" 
+            onClick={onBackClick} 
+            className="ml-[115px]" 
+          />
+          <Button 
+            texto="Siguiente" 
+            color="text-[#496ceb]" 
+            onClick={onNextClick} 
+            className="mr-[75px]" 
+          />
         </div>
       </div>
     </>
