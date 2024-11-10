4e281853cddd5978d251f15c661994b492d42678
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-11-10 15:23:39 +0000

    chore: translate to english

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-interface BotonProps {
+interface ButtonProps {
   texto: string;
   color: string; 
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   className?: string;
 }
 
-const Button: React.FC<BotonProps> = ({ texto, color, onClick, className }) => {
+const Button: React.FC<ButtonProps> = ({ texto, color, onClick, className }) => {
   return (
     <button
       onClick={onClick}
