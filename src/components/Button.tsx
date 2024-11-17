fb5b1bce20b540253f11160d84f775d3417f9c3b
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-11-17 04:38:19 +0000

    feat: add disabled state to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,13 +5,16 @@ interface ButtonProps {
   color: string; 
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   className?: string;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ texto, color, onClick, className }) => {
+const Button: React.FC<ButtonProps> = ({ texto, color, onClick, className, disabled }) => {
   return (
     <button
       onClick={onClick}
-      className={`text-center ${color} ${className} text-1xl font-normal font-['Roboto'] uppercase leading-tight tracking-tight`}
+      className={`text-center ${color} ${className} text-1xl font-normal font-['Roboto'] uppercase leading-tight tracking-tight ${
+        disabled ? 'opacity-50 cursor-not-allowed' : '' }`}
+      disabled={disabled}
     >
       {texto}
     </button>
