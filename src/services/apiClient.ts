0f144a59abfd078782315c785dc67c290e2ead6a
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-11-17 04:45:13 +0000

    feat: add endpoints for retrieving userId, uploading images, and fetching imageId

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -38,3 +38,50 @@ export const fetchUserProfile = async (token: string): Promise<ProfileData | nul
     return null;
   }
 };
+
+//Función para obtener el UserId
+export const fetchUserIdByEmail = async (email: string): Promise<string | null> => {
+  try {
+    const response = await axios.get(`/api/helphub/user/${email}`);
+    const user = response.data[0]; // Accede al primer elemento del array
+    return user ? user._id : null; // Devuelve el _id si existe 
+  } catch (error) {
+    console.error("Error fetching user ID:", error);
+    return null;
+  }
+};
+
+// Función para subir la imagen de perfil
+export const uploadProfileImage = async (file: File, userId: string): Promise<{ imageUrl: string }> => {
+  const formData = new FormData();
+  const token = localStorage.getItem('token');
+  formData.append('image_profile', file);
+  formData.append('id_user', userId); 
+
+  try {
+    const response = await axios.post('/api/helphub/upload-service/upload-profileImage', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+    return response.data; 
+  } catch (error) {
+    console.error("Error uploading profile image:", error);
+    throw error;
+  }
+};
+
+//Función para obtener el ImageId
+export const fetchImageId = async (imageId: string): Promise<string | null> => {
+  try {
+    const response = await axios.get(`/api/helphub/upload-service/profile-image/${imageId}`);
+    const image = response.data[0]; // Accede al primer elemento del array
+    return image ? image._id : null; // Devuelve el _id si existe 
+  } catch (error) {
+    console.error("Error fetching image ID:", error);
+    return null;
+  }
+};
+
+
