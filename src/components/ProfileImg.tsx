906f881d0ca69758b80632cb05470a3a09e51124
ReyesMorales <reyesmoralesdominguez@gmail.com>
2024-11-22 20:33:54 +0000

    refactor: update ProfileImg component to use hooks and improve fallback for profile pictures

--- src/components/ProfileImg.tsx
+++ src/components/ProfileImg.tsx
@@ -1,21 +1,22 @@
 import { useProfileImage } from "../hooks/useProfileImage";
 
 interface ProfileImgProps {
-  userId: string;
-  className?: string;
+  userId: string | null; 
+  className?: string; 
 }
 
-function ProfileImg({ userId, className }: ProfileImgProps) {
+const ProfileImg: React.FC<ProfileImgProps> = ({ userId, className }) => {
   const { profilePicture } = useProfileImage(userId);
+
   return (
     <div className={className}>
       <img
-        src={profilePicture}
-        alt="Perfil"
-        className={`w-full h-full object-cover`}
-      />      
+        src={profilePicture || "/images/default-placeholder.png"}
+        alt="Imagen de perfil"
+        className="w-full h-full object-cover"
+      />
     </div>
-  )
-}
-  
+  );
+};
+
 export default ProfileImg;
