bf4777d63e0d1cb8849761ace6bc154206fd01cd
Gloria R <norimaqidoe@hotmail.com>
2024-11-22 22:20:12 -0600

    feat: Message page layout when there are none

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import ChatMessage from "./ChatMessage";
 import ProfileImg from "./ProfileImg";
 import { Message } from "../types/types";
+import ChatBoxEmpty from "./ChatBoxEmpty";
+import { userContacts } from "../Variables/varibles";
 
 function ChatBox() {
   const {openRequestTab} = useChatContext();
@@ -28,34 +30,39 @@ function ChatBox() {
       <section className={`flex flex-col justify-between w-full h-full border-black-50  bg-white font-roboto ${openRequestTab? "rounded-none border-x-[1px]" : "rounded-xl border"}`}>
         <ChatBoxHeader />
         <div className="flex flex-col justify-end flex-1  overflow-auto px-10 py-5">
-          {!openRequestTab && 
-          messages.map((message, index) => {
-            const isLastInBlock =
-              index === messages.length - 1 ||
-              messages[index + 1].sender !== message.sender;
-
-            return (
-              <div key={message.id}>
-                <ChatMessage
-                  content={message.content}
-                  sender={message.sender}
-                />
-
-                {isLastInBlock && (
-                  <ProfileImg
-                    className={`w-[73px] h-[73px] rounded-full overflow-hidden mt-2 ${
-                      message.sender === "user" ? "ml-auto" : "mr-auto"
-                    }`}
-                    src={`${
-                      message.sender === "user"
-                        ? "https://i.pinimg.com/736x/d2/54/d9/d254d9cfca7c55d54020c02ed217c995.jpg"
-                        : "https://i.pinimg.com/736x/7c/01/da/7c01da1f0562de36833d35d5530187a8.jpg"
-                    }`}
+          {userContacts.length === 0 ? (
+            <ChatBoxEmpty />
+          ) : (
+            !openRequestTab && 
+            messages.map((message, index) => {
+              const isLastInBlock =
+                index === messages.length - 1 ||
+                messages[index + 1].sender !== message.sender;
+  
+              return (
+                <div key={message.id}>
+                  <ChatMessage
+                    content={message.content}
+                    sender={message.sender}
                   />
-                )}
-              </div>
-            );
-          }
+  
+                  {isLastInBlock && (
+                    <ProfileImg
+                      className={`w-[73px] h-[73px] rounded-full overflow-hidden mt-2 ${
+                        message.sender === "user" ? "ml-auto" : "mr-auto"
+                      }`}
+                      src={`${
+                        message.sender === "user"
+                          ? "https://i.pinimg.com/736x/d2/54/d9/d254d9cfca7c55d54020c02ed217c995.jpg"
+                          : "https://i.pinimg.com/736x/7c/01/da/7c01da1f0562de36833d35d5530187a8.jpg"
+                      }`}
+                    />
+                  )}
+                </div>
+              );
+            }
+            )
+
           )}
         </div>
         {!openRequestTab && <ChatBoxInput onSendMessage={addMessage}/>}
--- src/components/InboxContacts.tsx
+++ src/components/InboxContacts.tsx
@@ -3,6 +3,7 @@ import { useChatContext } from "../context/ChatContext";
 import { userContacts } from "../Variables/varibles";
 import InboxContact from "./InboxContact"
 import InboxRequest from "./InboxRequest";
+import InboxEmpty from "./InboxEmpty";
 
 function InboxContacts() {
   const {openSms, openRequest} = useChatContext();
@@ -11,26 +12,28 @@ function InboxContacts() {
     <div className="flex flex-col items-start h-3/4 gap-4 shrink-0 rounded-md border border-black-50 pt-10">
       <p className="text-xl text-neutral-black font-medium leading-6 tracking-wide flex-[1_0_0%] px-[27px]">{`${openRequest ? "Solicitud de mensaje" : "Chats"}`}</p>
       <div className="flex flex-col h-full w-full">
-        {openSms ? 
-          userContacts.map((contact) => (
-            <InboxContact
-              key={contact.id}
-              id={contact.id}
-              src={contact.src}
-              message={contact.message}
-              name={contact.name}
-              selected={selected}
-              setSelected={setSelected}
-            />
+        {openSms ? (
+          userContacts.length === 0 ? (
+            <InboxEmpty />
+          ) : (
+            userContacts.map((contact) => (
+              <InboxContact
+                key={contact.id}
+                id={contact.id}
+                src={contact.src}
+                message={contact.message}
+                name={contact.name}
+                selected={selected}
+                setSelected={setSelected}
+              />
+            ))
           )
-          ) 
-          : 
-          (
-            <InboxRequest />
-          )}
+        ) : (
+          <InboxRequest />
+        )}
       </div>
     </div>
-  )
+  );
 }
 
 export default InboxContacts
